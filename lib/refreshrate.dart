210f264f91de7ebcfb24a8fa5fdf66151e1ac628
Anthony Louis <anthony@simbioseventures.com>
2019-09-05 13:16:50 -0300

    change list to a more beatiful form

--- lib/refreshrate.dart
+++ lib/refreshrate.dart
@@ -10,16 +10,30 @@ class RefreshRateList extends StatefulWidget{
 class RefreshRateListState extends State <RefreshRateList>{
   static final String routeName = '/refreshrate';
 
-	final List<String> list = ["Every 30 seconds", "Every Minute", "Every 5 Minutes", "Every 15 Minutes", "Every 30 minutes",  "Every Hour", "Every 24 hours" ];
-	final Map refreshRate = { 'Every 30 seconds': 30, 'Every Minute': 60, 'Every 5 Minutes': 300, 'Every 15 Minutes': 900, 'Every 30 minutes': 1800, 'Every Hour': 3600,'Every 24 hours': 90000 };
-
+  final List<String> list = [
+    "Every 30 seconds",
+    "Every Minute",
+    "Every 5 Minutes",
+    "Every 15 Minutes",
+    "Every 30 minutes",
+    "Every Hour",
+    "Every 24 hours"
+  ];
+  final Map refreshRate = {
+    'Every 30 seconds': 30,
+    'Every Minute': 60,
+    'Every 5 Minutes': 300,
+    'Every 15 Minutes': 900,
+    'Every 30 minutes': 1800,
+    'Every Hour': 3600,
+    'Every 24 hours': 90000
+  };
 
   void _savePrefs( int refreshRate ) async{
 
     SharedPreferences prefs = await SharedPreferences.getInstance();
     
     prefs.setInt('refreshRate', refreshRate);
-    
   }
 
   @override
